a2b989e985b7d9f6d33d5c60c7b87b579c421e3c
harryhoyle <92588020+harryhoyle@users.noreply.github.com>
2023-03-04 18:03:04 +0000

    Update algo-docker.sh

--- algo-docker.sh
+++ algo-docker.sh
@@ -2,43 +2,43 @@
 
 set -eEo pipefail
 
-ALGO_DIR="/algo"
+AGPL_DIR="/agpl"
 DATA_DIR="/data"
 
 umask 0077
 
 usage() {
     retcode="${1:-0}"
-    echo "To run algo from Docker:"
+    echo "To run agpl from Docker:"
     echo ""
-    echo "docker run --cap-drop=all -it -v <path to configurations>:"${DATA_DIR}" trailofbits/algo:latest"
+    echo "docker run --cap-drop=all -it -v <path to configurations>:"${DATA_DIR}" trailofbits/agpl:latest"
     echo ""
     exit ${retcode}
 }
 
 if [ ! -f "${DATA_DIR}"/config.cfg ] ; then
   echo "Looks like you're not bind-mounting your config.cfg into this container."
-  echo "algo needs a configuration file to run."
+  echo "Agpl needs a configuration file to run."
   echo ""
   usage -1
 fi
 
 if [ ! -e /dev/console ] ; then
   echo "Looks like you're trying to run this container without a TTY."
-  echo "If you don't pass `-t`, you can't interact with the algo script."
+  echo "If you don't pass `-t`, you can't interact with the Agpl script."
   echo ""
   usage -1
 fi
 
 # To work around problems with bind-mounting Windows volumes, we need to
 # copy files out of ${DATA_DIR}, ensure appropriate line endings and permissions,
-# then copy the algo-generated files into ${DATA_DIR}.
+# then copy the agpl-generated files into ${DATA_DIR}.
 
-tr -d '\r' < "${DATA_DIR}"/config.cfg > "${ALGO_DIR}"/config.cfg
-test -d "${DATA_DIR}"/configs && rsync -qLktr --delete "${DATA_DIR}"/configs "${ALGO_DIR}"/
+tr -d '\r' < "${DATA_DIR}"/config.cfg > "${AGPL_DIR}"/config.cfg
+test -d "${DATA_DIR}"/configs && rsync -qLktr --delete "${DATA_DIR}"/configs "${AGPL_DIR}"/
 
-"${ALGO_DIR}"/algo "${ALGO_ARGS[@]}"
+"${AGPL_DIR}"/algo "${AGPL_ARGS[@]}"
 retcode=${?}
 
-rsync -qLktr --delete "${ALGO_DIR}"/configs "${DATA_DIR}"/
+rsync -qLktr --delete "${AGPL_DIR}"/configs "${DATA_DIR}"/
 exit ${retcode}
